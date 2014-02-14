3d8a8a40efdf94f182cda89c14286be243d3d25f
Bob Martens <bob@bobmartens.net>
2014-02-14 09:28:16 -0600

    Initial Commit

---
+++ CarValet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CarValet
+//
+//  Created by Bob Martens on 2/14/14.
+//  Copyright (c) 2014 Bob Martens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CarValet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CarValet
+//
+//  Created by Bob Martens on 2/14/14.
+//  Copyright (c) 2014 Bob Martens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
